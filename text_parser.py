7628cb80a4be5a132162b519280eeb2de49863d7
George Moreas <gmoreasjr@gmail.com>
2015-09-21 22:41:56 -0700

    Initial code for the parser

---
+++ text_parser.py
@@ -0,0 +1,18 @@
+import BeautifulSoup
+import urllib2
+
+
+def a():
+    response = urllib2.urlopen('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program')
+    html = response.read()
+    soup = BeautifulSoup.BeautifulSoup(html)
+    div = soup.find(id="mw-content-text")
+
+    all_p = []
+
+    for p in div.findAll('p'):
+        inner_p = p.text
+        all_p.append(inner_p)
+        all_p.append(' ')
+
+    print all_p
