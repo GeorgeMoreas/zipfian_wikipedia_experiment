acd277e474f9b700bdebd9b3bec5323bb7b831cf
George Moreas <gmoreasjr@gmail.com>
2015-09-21 23:40:09 -0700

    Now strips off all non-alphanum + spaces

--- text_parser.py
+++ text_parser.py
@@ -1,6 +1,7 @@
 import BeautifulSoup
 import urllib2
-
+import unicodedata
+import string, re
 
 def a():
     response = urllib2.urlopen('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program')
@@ -12,8 +13,28 @@ def a():
 
     for p in div.findAll('p'):
         inner_p = p.getText(separator=u' ')
+        pattern = re.compile('[^a-zA-Z0-9\s]')
+        inner_p = pattern.sub('', inner_p)
         all_p.append(inner_p)
-        all_p.append(' ')
 
-    print all_p
+#    all_words = all_p[0].split()
+#    all_words = [i.split()[0] for i in all_p]
+#    all_words = all_p
+
+    all_words = []
+
+    for p in all_p:
+        all_words.append(p.split())
+
+    word_count = {}
+    x = 0
+    for word in all_words:
+        if word in word_count.keys():
+            x = 5
+#            word_count[word] += 1
+        else:
+            x = 3
+#            word_count[word] = 1
+
+    print all_words[0]
 
